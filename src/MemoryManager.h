fb71c93c77434d6d9003dc77dbed1403f5033029
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2017-10-29 23:13:04 +0100

    removed ifdef in memory manager

--- src/MemoryManager.h
+++ src/MemoryManager.h
@@ -36,11 +36,9 @@ private:
 
 /* ------------------------------------------------ */
 
-#ifdef DEVELOP
 void* operator new(size_t size);
 void* operator new [](size_t size);
 void operator delete(void * p);
 void operator delete [](void * p);
-#endif
 
 #endif
