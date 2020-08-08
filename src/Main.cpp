f9c5215be5381572e2515a95578539ee6a59bc39
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2020-08-08 02:29:35 +0200

    Disable print buf so that lua script doest block print to terminal

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,88 +8,103 @@
 #define LUA_DB_VERSION "0.1.0.0"
 
 int run(int argc, char* argv[]) {
-	CliArgs args;
-	CliArgs::parse(argc, argv, &args);
-	const String uri = MongoManager::getUri(args.host, args.port);
+    CliArgs args;
+    CliArgs::parse(argc, argv, &args);
+    const String uri = MongoManager::getUri(args.host, args.port);
 
-	args.database = "test";
+    args.database = "test";
 
-	printf("LuaDB shell version: %s\n", LUA_DB_VERSION);
-	if (args.database.size()) {
-		printf("Connecting to : %s/%s\n", uri.c_str(), args.database.c_str());
-	}
-	else {
-		printf("Connecting to : %s\n", uri.c_str());
-	}
+    printf("LuaDB shell version: %s\n", LUA_DB_VERSION);
+    if (args.database.size()) {
+        printf("Connecting to : %s/%s\n", uri.c_str(), args.database.c_str());
+    }
+    else {
+        printf("Connecting to : %s\n", uri.c_str());
+    }
 
-	LuaManager *pLua = LuaManager::getInstance();
+    LuaManager* pLua = LuaManager::getInstance();
 
-	bson_t reply;
-	if (!pLua->getMongo()->connect(uri, &reply)) {
-		printf("Error: couldn't connect to server, connection attempt failed\n");
-		return -1;
-	}
+    bson_t reply;
+    if (!pLua->getMongo()->connect(uri, &reply)) {
+        printf("Error: couldn't connect to server, connection attempt failed\n");
+        return -1;
+    }
 
-	bson_iter_t it;
-	if (bson_iter_init_find(&it, &reply, "version")) {
-		printf("MongoDB server version: %s\n", bson_iter_utf8(&it, nullptr));
-	}
-	bson_destroy(&reply);
+    bson_iter_t it;
+    if (bson_iter_init_find(&it, &reply, "version")) {
+        printf("MongoDB server version: %s\n", bson_iter_utf8(&it, nullptr));
+    }
+    bson_destroy(&reply);
 
-	if (args.database.size()) {
-		pLua->useDatabase(args.database);
-	}
+    if (args.database.size()) {
+        pLua->useDatabase(args.database);
+    }
 
 
-//	pLua->loadFile("../test/test_bsonTypes.lua");
-//	pLua->loadFile("../test/test_crud.lua");
-	//pLua->loadFile("../test/performance.lua");
-	
+    //	pLua->loadFile("../test/test_bsonTypes.lua");
+    //	pLua->loadFile("../test/test_crud.lua");
+        //pLua->loadFile("../test/performance.lua");
 
-	if (args.files.size()) {
-		for (int i = 0; i < args.files.size(); ++i) {
-			printf("\nLoading file: %s\n\n", args.files[i].c_str());
-			pLua->loadFile(args.files[i]);
-		}
-		if (args.shell) {
-			printf("\n");
-		}
-		pLua->setIsRunning(args.shell);
-	}
+    //    fflush(stdout);
 
-	if (pLua->isRunning()) {
-		printf("Type \"help()\" for help\n");
-		while (pLua->isRunning()) {
-			printf("> ");
-			String commandStr;
-			String inputStr;
-			if (std::getline(std::cin, inputStr)) {
-				commandStr += inputStr;
-//				while (_kbhit()) {
-//					printf("... ");
-//					std::getline(std::cin, inputStr);
-//					commandStr += inputStr + "\n";
-//				}
-			}
-			if (!commandStr.empty()) {
-				pLua->loadString(commandStr.c_str());
-			}
-		}
-	}
 
-	return 0;
+    if (args.files.size()) {
+
+ /*       printf("111111");
+        fflush(stdout);*/
+
+        for (int i = 0; i < args.files.size(); ++i) {
+            printf("\nLoading file: %s\n\n", args.files[i].c_str());
+            pLua->loadFile(args.files[i]);
+        }
+
+     /*   printf("222222");
+        fflush(stdout);*/
+
+        if (args.shell) {
+            printf("\n");
+            //fflush(stdout);
+        }
+        pLua->setIsRunning(args.shell);
+    }
+
+    if (pLua->isRunning()) {
+        printf("Type \"help()\" for help\n");
+        while (pLua->isRunning()) {
+            printf("> ");
+            String commandStr;
+            String inputStr;
+            if (std::getline(std::cin, inputStr)) {
+                commandStr += inputStr;
+                //				while (_kbhit()) {
+                //					printf("... ");
+                //					std::getline(std::cin, inputStr);
+                //					commandStr += inputStr + "\n";
+                //				}
+            }
+            if (!commandStr.empty()) {
+                pLua->loadString(commandStr.c_str());
+            }
+        }
+    }
+
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
-	run(argc, argv);
-	LuaManager::destroyInstance();
-	#ifdef DEBUG_MEMORY
-		if (MemoryManager::getInstance()->hasLeak()) {
-			printf("\n");
-			MemoryManager::getInstance()->displayInfo();
-			getchar();
-			return 1;
-		}
-	#endif
-	return 0;
+    //Disable print buffer
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+    run(argc, argv);
+    LuaManager::destroyInstance();
+#ifdef DEBUG_MEMORY
+    if (MemoryManager::getInstance()->hasLeak()) {
+        printf("\n");
+        MemoryManager::getInstance()->displayInfo();
+        getchar();
+        return 1;
+    }
+#endif
+    //getchar();
+    return 0;
 }
